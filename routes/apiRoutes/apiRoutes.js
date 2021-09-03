fad810f69d59c9048974ccd2615f63d2cdc4363b
Tony Rivera <ajboricua@gmail.com>
2021-09-02 21:29:30 -0400

    Fixed server.js and apiRoute.js

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,5 +1,10 @@
+const {
+    readFile,
+    writeFile
+} = require('fs');
+const { join } = require('path');
 const path = require('path');
-// const { v4 } = require('uuid');
+const { v4 } = require('uuid');
 const router = require('express').Router();
 
 // The following API routes should be created:
@@ -14,16 +19,81 @@ const router = require('express').Router();
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-router.get('/notes', (req, res) => {
-    res.send('welcome!');
-    
-})
+// router.get('/notes', (req, res) => {
+//     res.send('welcome!');
 
-router.post('/notes', (req, res) => {
-        res.send('welcome!');
-})
+// })
+
+// router.post("/notes", (req, res) => {
+//     readFile("", "utf8", function (err, data) {
+//         let noteData = [];
+//         if (err) {
+//             throw err;
+//         }
+//     })
+// })
+
+
+
+// app.get
+router.get("/notes", (req, res) => {
+    readFile("./db/db.json", "utf8", function (err, data) {
+        let apiNote = [];
+        if (err) {
+            throw err;    
+        };
+        apiNote = JSON.parse(data);
+        return res.json(apiNote);
+    });
+});
 
 // router.post
+
+router.post("/notes", (req, res) => {
+    let postNotes = req.body;
+    readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) {
+            console.log('err at database ${err}');
+        } else {
+            if (data.length > 0) {
+                noteObj = JSON.parse(data);
+            } else {
+                noteObj = [];
+            };
+            postNotes.id = v4().substring(0);
+            noteObj.push(postNotes);
+        }
+        writeFile("./db/db.json", JSON.stringify(noteObj), "utf8", (err) => {
+            if (err)
+                throw err;
+            return res.json(postNotes);
+        })
+    })
+});
+
+// DELETE /api/notes/:id
+router.delete("/notes/:id", (req, res) => {
+    readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        let delObject = JSON.parse(data);
+        // parse an immutable database.json 
+        const deleteThis = delObject.findIndex((note) => note.id === req.params.id);
+
+        delObject.splice(deleteThis, 1);
+        const postObject = writeFile("./db/db.json", JSON.stringify(delObject), (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log("Note deleted");
+        });
+        res.send(postObject);
+    });
+
+});
+
+
 
 
 
--- server.js
+++ server.js
@@ -5,26 +5,22 @@ const path = require('path');
 
 const apiRoutes = require('./routes/apiRoutes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes.js');
-const PORT = process.env.PORT || 3003;
+const { readFile, writeFile } = require('fs');
+const router = require('./routes/apiRoutes/apiRoutes.js');
+const PORT = process.env.PORT || 3008;
 const app = express();
 
+
+// routes 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
-
-
+// listening to the ports
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!');
 })
-
-
-// Getting Started
-// The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module.
-
-
-// Bonus
-// You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. As a bonus, see if you can add the DELETE route to the application using the following guideline:
-
-// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
